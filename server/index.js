86b3ee4b99783471d0fe4252798b8c6e6df0dd23
Adnan Khan <adnan.khan2019@vitbhopal.ac.in>
2022-07-25 20:41:08 +0530

    connected to mogobd server✅

--- server/index.js
+++ server/index.js
@@ -1,4 +1,21 @@
-import Express from "express";
+import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import Cors  from "cors";
+import cors  from "cors";
+
+// initializing app
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+// connecting to the MongoDB Database
+const CONNECTION_URL = 'mongodb+srv://Ady:1234@cluster0.hlmjk.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+//mongoose.set('useFindAndModify', false); // its no longer supported as by default mongoose takes its value as false
