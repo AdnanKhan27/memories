055d0aea9b4d701592f861afc417812906221d8a
Adnan Khan <adnan.khan2019@vitbhopal.ac.in>
2022-07-27 17:05:53 +0530

    added routes and controllers

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,18 @@
 // creating handlers for our routes here to simplify the code
-export const getPosts = (req, res) => {
-    res.send('This Works!');
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find();
+
+        console.log(postMessages);
+
+        res.status(200).json(postMessages);
+    } catch (error){
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const createPost = (req, res) => {
+    res.send('Post Creation');
 }
